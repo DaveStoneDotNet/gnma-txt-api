d7b18a5f48acd15f96dc8c274bb7f96ca1beff12
Dave Stone <dave.stone@mrcooper.com>
2019-01-18 16:46:18 -0600

    first commit

---
+++ Startup.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
+
+using Cooper.Gnma.Text.Models;
+
+namespace Cooper.Gnma.Text.Api
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            // TODO: MVC now serializes JSON with camel case names by default. Call 'AddJsonOptions' as shown below to avoid camel case names by default.
+            // https://stackoverflow.com/questions/38202039/json-properties-now-lower-case-on-swap-from-asp-net-core-1-0-0-rc2-final-to-1-0
+            // https://github.com/aspnet/Announcements/issues/194
+            // https://stackoverflow.com/questions/38139607/asp-net-core-1-0-web-api-use-camelcase
+            
+            // A default camel case would be preferred, however, there's a bit of existing code that does NOT implement the camel case transform that would need to be re-factored
+
+            services.AddMvc().AddJsonOptions(options => options.SerializerSettings.ContractResolver = new Newtonsoft.Json.Serialization.DefaultContractResolver());
+            services.AddOptions();
+            services.Configure<AppSettings>(Configuration.GetSection("AppSettings"));
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseCors(builder => builder
+               .AllowAnyOrigin()
+               .AllowAnyMethod()
+               .AllowAnyHeader()
+               .AllowCredentials()); 
+
+            app.UseMvc();
+        }
+    }
+}
